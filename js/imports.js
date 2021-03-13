b0e056302a265cfc0d080d569ef5618403091d37
vallemkr <vallemkr@umich.edu>
2021-03-13 03:43:02 -0500

    switched everything to vue js because of react issues and potential jinja2 integration difficulties

--- js/imports.js
+++
@@ -1,7 +0,0 @@
-// Every other library is imported in the index.html file as cdn script tag links
-
-// htm is a library that allows you to use jsx-like syntax with string templates
-// it is nice because you don't need to use a build step like you would with normal jsx
-// I couldn't figure out how to import it as a script tag in the html, so I'm importing it here instead
-import htm from "https://unpkg.com/htm@3.0.4/dist/htm.module.js?module"
-export const html = htm.bind(React.createElement);
