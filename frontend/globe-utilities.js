154c274236a2d56c31191393b442f53bb2d6b0b4
Krithik Vallem <47306032+KrithikVallem@users.noreply.github.com>
2021-09-29 20:18:08 -0400

    Cosmetic change - Reduced line count

--- frontend/globe-utilities.js
+++ frontend/globe-utilities.js
@@ -7,7 +7,6 @@ class Globe {
         this.markerClickedFunction = null; // this will be set after the VueApp is created
 
         //== actual constructor stuff begins here ==
-
         this.initializeGlobe(); // sets this.globe
         this.initializeMarkers(allArticles); // sets this.allMarkers
         // display active markers for every article
@@ -15,62 +14,47 @@ class Globe {
     }
 
     initializeGlobe() {
-        // equivalent to the documentation's initialize() function
-        const globe = new WE.map("globe-div", {
-            sky: true, // adds stars to background
-        });
-        //WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(globe);
-        WE.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}').addTo(globe)
+        // call's the WebGL-Earth library's initialize() function
+        const globe = new WE.map("globe-div", { sky: true });
 
+        WE.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}').addTo(globe)
         globe.setZoom(this.zoomLevel);
         
         // Start a simple rotation animation
-        // http://examples.webglearth.com/#animation
+        // Mostly copied from the documentation found here => http://examples.webglearth.com/#animation
         var before = null;
         requestAnimationFrame(function animate(now) {
-
-            // this line isn't in the documentation
-            // I added it myself to change rotation speed depending on zoom level
-            // suggested by Anthony Marx to make it easier to click markers when zoomed in
-            // original rotation speed in documention example was hardcoded as 0.1
+            // These 2 lines aren't in the documentation
+            // I added it to change rotation speed depending on zoom level in order to make it easier to click markers when zoomed in
+            // Original rotation speed in documention example was hardcoded as 0.1
             // Math.min is used because otherwise the globe spins crazy fast when you zoom out too far
             const z = globe.getZoom();
-            const rotationSpeed = Math.min( 0.1,
-                1 / ( 2 * z * z )
-            );
-
+            const rotationSpeed = Math.min( 0.1, 1 / ( 2 * z * z ) );
+            
             var c = globe.getPosition();
             var elapsed = before? now - before: 0;
             before = now;
             globe.setCenter([c[0], c[1] - rotationSpeed*(elapsed/30)]);
             requestAnimationFrame(animate);
         });
-
         this.globe = globe;
     }
 
-    // https://github.com/pointhi/leaflet-color-markers
-    // create green active and grey inactive markers
+    // create green active and grey inactive markers - images from https://github.com/pointhi/leaflet-color-markers
     createMarker(locationName, coordinates, isActive, articleURL) {
         const iconURL = isActive ? this.activeIconImage : this.inactiveIconImage;
-        
         const newMarker = WE.marker(coordinates, iconURL).bindPopup(locationName, 75);
-
         newMarker.element.onclick = () => {
             // close the popup 2.5 seconds after the marker is clicked
             setTimeout(() => newMarker.closePopup(), 2500);
-
             // needs to be set from outside, after the VueApp is created
             this.markerClickedFunction(articleURL, coordinates);
         }
-
         return newMarker;
     }
 
-    // create markers for each location in each article
-    // and place them in an object to be retrieved later
+    // create markers for each location in each article and place them in an object to be retrieved later
     initializeMarkers(allArticles) {
-        // maps article url => markers for that url
         this.allMarkers = {};
 
         for (const article of allArticles) {
@@ -78,15 +62,12 @@ class Globe {
 
             for (const locationName in article.locations) {
                 const coordinates = article.locations[locationName];
-
                 const newMarker = this.createMarker(locationName, coordinates, true, article.url);
-
                 this.allMarkers[ article.url ].push(newMarker);
             }
         }
     }
 
-    // remove all existing markers from the globe
     clearAllMarkers() {
         for (const url in this.allMarkers) {
             for (const marker of this.allMarkers[url]) {
@@ -102,14 +83,11 @@ class Globe {
             for (const article of articles) {
                 for (const marker of this.allMarkers[ article.url ]) {
                     // look at the html structure to understand this
-                    marker.element.querySelector(".we-pm-icon")
-                        .style.backgroundImage = `url("${iconImage}")`;
-
+                    marker.element.querySelector(".we-pm-icon").style.backgroundImage = `url("${iconImage}")`;
                     marker.addTo(this.globe);
                 }
             }
         }
-
         // render inactive first, so active markers at same location will appear above them
         displayMarkers(inactiveArticles, this.inactiveIconImage);
         displayMarkers(activeArticles, this.activeIconImage);
